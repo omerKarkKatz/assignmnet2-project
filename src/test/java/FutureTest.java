3b5fd8394ab0e484ba46bbd1606e4001e61e9db1
omerkark <omerkark@gmail.com>
2018-11-27 14:01:24 +0200

    future test

--- src/test/java/FutureTest.java
+++ src/test/java/FutureTest.java
@@ -2,34 +2,47 @@ import bgu.spl.mics.Future;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 public class FutureTest {
 
     private Future<Object> future;
-    private Object o;
+    private Object result;
 
     @Before
     public void setUp() throws Exception {
         future = new Future();
-        o = new Object();
+        result = new Object();
     }
 
     @Test
     public void get() {
-        future.get();
-        System.out.println("hello");
+        future.resolve(result);
+        assertEquals(result, future.get());
     }
 
     @Test
     public void resolve() {
+        future.resolve(result);
+        assertEquals("resolved different than should",result,future.get());
     }
 
     @Test
     public void isDone() {
+        assertFalse(future.isDone());
+        future.resolve(result);
+        assertTrue(future.isDone());
     }
 
     @Test
     public void get1() {
+        assertNull(future.get(0, TimeUnit.MILLISECONDS));
+        assertNull(future.get(100,TimeUnit.MILLISECONDS));
+        future.resolve(result);
+
+
+
     }
 }
