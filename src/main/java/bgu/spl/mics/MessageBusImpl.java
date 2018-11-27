dd338fe11ed70553291bc351872ab978f454e32e
omerkark <omerkark@gmail.com>
2018-11-27 20:52:49 +0200

    Inventory Test

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,11 +1,16 @@
 package bgu.spl.mics;
 
+import java.util.HashMap;
+import java.util.Queue;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
 public class MessageBusImpl implements MessageBus {
+
+	private HashMap< MicroService ,Queue<Event>> MicroServisesQueueu;
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
