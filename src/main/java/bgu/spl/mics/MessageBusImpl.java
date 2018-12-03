83a63203a8d21217fcf0686f64b9fc25b13adf86
omerkark <omerkark@gmail.com>
2018-12-03 08:20:36 +0200

    Merge branch 'master' of /home/omer/IdeaProjects/assignmnet2-project with conflicts.

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,61 +1,139 @@
 package bgu.spl.mics;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
 
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
-public class MessageBusImpl implements MessageBus {
+    public class MessageBusImpl implements MessageBus {
+
+        private static class SingletonHolder {
+            private static MessageBusImpl MessegeBusInstance = new MessageBusImpl();
+        }
+
+        private ConcurrentHashMap<MicroService, LinkedBlockingQueue<Message>> QueueOfMicroTasks;
+        private ConcurrentHashMap<Class<? extends Event>, LinkedBlockingQueue<MicroService>> eventsSubscribers;
+        private ConcurrentHashMap<Class<? extends Broadcast>, Vector<MicroService>> broadcastSubscribers;
+        private ConcurrentHashMap<Event, Future> EventToFuture;
+        private ConcurrentHashMap<MicroService, Vector<Class<? extends Message>>> messagesOfMicroToDelete;
+        private Object lockSendTask;
+		Semaphore read_q = new Semaphore(1);
+		Semaphore edit_q = new Semaphore(1);
+
+	private MessageBusImpl(){
+	    QueueOfMicroTasks = new ConcurrentHashMap<>();
+	    eventsSubscribers = new ConcurrentHashMap<>();
+	    broadcastSubscribers = new ConcurrentHashMap<>();
+	    EventToFuture = new ConcurrentHashMap<>();
+		messagesOfMicroToDelete = new ConcurrentHashMap<>();
+	}
+
+	public static MessageBus getInstance() {
+		return SingletonHolder.MessegeBusInstance;
+	}
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// TODO Auto-generated method stub
+		// check if to sync these 2 lines.
+		if(QueueOfMicroTasks.contains(m)) {
+			messagesOfMicroToDelete.putIfAbsent(m, new Vector<>());
+			messagesOfMicroToDelete.get(m).add(type);
 
+			eventsSubscribers.putIfAbsent(type, new LinkedBlockingQueue<>());
+			eventsSubscribers.get(type).add(m);
+		}
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
+		if(QueueOfMicroTasks.contains(m)) {
+			messagesOfMicroToDelete.putIfAbsent(m, new Vector<>());
+			messagesOfMicroToDelete.get(m).add(type);
 
+			//check if this method should be synchonized
+			broadcastSubscribers.putIfAbsent(type, new Vector<>());
+			broadcastSubscribers.get(type).add(m);
+		}
 	}
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
-
+		// find the futere object related to event e, resolve the future obj(future method)
+		EventToFuture.get(e).resolve(result);
+		// check if to delete the event from the hash or you need it fot log.
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
-
+		q = broadcastSubscribers.get(b);
+		synchronized (q) {
+			Iterator<MicroService> iter = q.iterator();
+			while (iter.hasNext()) {
+				synchronized (lockSendTask) {
+					QueueOfMicroTasks.get(iter.next()).add(b);
+				}
+			}
+		}
 	}
 
-	
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
+		Future<T> futureEvent = new Future<T>();
+		EventToFuture.putIfAbsent(e, futureEvent);
+		MicroService m = null;
+		if(eventsSubscribers.contains(e)) {
+			synchronized (eventsSubscribers.get(e)) {
+				m = eventsSubscribers.get(e).poll();
+				// checkes if there is a micro service that can handle this.
+				if (m != null)
+					return null;
+					// moves the micro to the end of the queue (round robin manner), adds message to the microMessageQueue.
+				else {
+					eventsSubscribers.get(e).add(m);
+					synchronized (lockSendTask) {
+						QueueOfMicroTasks.get(m).add(e);
+					}
+				}
+			}
+		}
+			return futureEvent;
 	}
 
 	@Override
 	public void register(MicroService m) {
-		// TODO Auto-generated method stub
-
+		QueueOfMicroTasks.putIfAbsent(m, new LinkedBlockingQueue<Message>());
 	}
+
 
 	@Override
 	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
-
+		// check if clear is sync.
+		QueueOfMicroTasks.get(m).clear();
+		QueueOfMicroTasks.remove(m);
+		Iterator<Class<? extends Message>> iter = messagesOfMicroToDelete.get(m).iterator();
+		while(iter.hasNext()) {
+			Class<? extends Message> message = iter.next();
+			if (message.getClass().equals(Event.class))
+				q = eventsSubscribers.get(message);
+				synchronized (q){
+					q.remove(m);
+				}
+			else
+				broadcastSubscribers.get(message).remove(m);
+		}
+		m.terminate();
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+			if (!QueueOfMicroTasks.contains(m))
+				throw new IllegalStateException();
+			else
+				return QueueOfMicroTasks.get(m).take();
 	}
-
-	
-
 }
